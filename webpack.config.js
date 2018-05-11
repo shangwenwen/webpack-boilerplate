b9f1a5a4ec1eff7125ea91c23f3749026e8ab8ac
shangwenwen <1967553097@qq.com>
2018-05-12 07:16:31 +0800

    all

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// postcss
 const postcssConfig = require('./postcss.config.js');
 
 /* eslint-disable */
